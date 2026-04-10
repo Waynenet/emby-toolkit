526fc9735caeb955b55fc924a06fa5d7fd3c97bc
Wayne <way8941@gmail.com>
2026-04-10 16:47:57 +0800

    feat: 移除github令牌

--- emby-actor-ui/src/components/settings/GeneralSettingsPage.vue
+++ emby-actor-ui/src/components/settings/GeneralSettingsPage.vue
@@ -223,10 +223,6 @@
                         </n-text>
                       </n-space>
                     </n-form-item>
-                    <n-form-item label="GitHub 个人访问令牌" path="github_token">
-                      <n-input type="password" show-password-on="mousedown" v-model:value="configModel.github_token" placeholder="可选，用于提高API请求频率限制"/>
-                      <template #feedback><n-text depth="3" style="font-size:0.8em;"><a href="https://github.com/settings/tokens/new" target="_blank" style="font-size: 1.3em; margin-left: 4px; color: var(--n-primary-color); text-decoration: underline;">免费申请GithubTOKEN</a></n-text></template>
-                    </n-form-item>
                     <n-form-item label="启用在线豆瓣API" path="douban_enable_online_api">
                       <n-space align="center">
                         <n-switch v-model:value="configModel.douban_enable_online_api" />
