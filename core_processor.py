cc1484d6728f79a52723750c9b1abbc171a18067
hbq0405 <hbq0405@hotmail.com>
2026-07-19 17:05:51 +0800

    修复主动入库重复重选图片

--- core_processor.py
+++ core_processor.py
@@ -2718,7 +2718,7 @@ class MediaProcessor:
                     emby_api_key=self.emby_api_key,
                     user_id_for_ops=self.emby_user_id,
                     replace_all_metadata_param=uses_etk_metadata_provider,
-                    replace_all_images_param=uses_etk_metadata_provider and not specific_episode_ids,
+                    replace_all_images_param=False,
                     item_name_for_log=item_name_for_log
                 )
                 if uses_etk_metadata_provider:
